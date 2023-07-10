a16b7fc1198ef495011b28cfce7d3c5bd309168b
LightYe4r <24adrenalin@naver.com>
2023-07-10 20:49:18 +0900

    FIX : CORS

--- project/project/settings.py
+++ project/project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,13 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = ['https://port-0-toy-k19y2kljwq5eju.sel4.cloudtype.app/',
                         'https://localhost:3000/',
                         'https://127.0.0.1:8000/',
-                        '*',]
+                        ]
 CSRF_TRUSTED_ORIGINS = ['https://port-0-toy-k19y2kljwq5eju.sel4.cloudtype.app/']
 
 ROOT_URLCONF = 'project.urls'
