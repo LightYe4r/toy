1e782ab8e9da29a1c38a04e5977c1fcc6d04f0f3
LightYe4r <24adrenalin@naver.com>
2023-07-10 20:51:07 +0900

    FIX : CORS

--- project/project/settings.py
+++ project/project/settings.py
@@ -59,6 +59,12 @@ CORS_ALLOWED_ORIGINS = ['https://port-0-toy-k19y2kljwq5eju.sel4.cloudtype.app/',
                         ]
 CSRF_TRUSTED_ORIGINS = ['https://port-0-toy-k19y2kljwq5eju.sel4.cloudtype.app/']
 
+CORS_ORIGIN_WHITELIST = ['https://port-0-toy-k19y2kljwq5eju.sel4.cloudtype.app/',
+                        'https://localhost:3000/',
+                        'https://127.0.0.1:8000/',
+                        ]
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
