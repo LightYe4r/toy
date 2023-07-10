ab8b3905cee1aea0e42f01764302a0e5c7eceee6
LightYe4r <24adrenalin@naver.com>
2023-07-10 20:18:16 +0900

    FIX : CORS

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%=z3i=u1t+qx-u&13u6(3^#)4rtf_^db+dc_(e3_le!i5xkbue
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0:8000', 'localhost:8000', '127.0.0.1']
+ALLOWED_HOSTS = ['http://127.0.0.1:3000', 'http://localhost:3000', '0.0.0.0:8000', 'localhost:8000', '127.0.0.1']
 
 
 # Application definition
