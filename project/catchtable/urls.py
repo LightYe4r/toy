68524eb7fec4039c40b06ebfce6d2ac4d41daf86
LightYe4r <24adrenalin@naver.com>
2023-07-13 04:49:18 +0900

    ADD : reservation

--- project/catchtable/urls.py
+++ project/catchtable/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import re_path as url
 from .views import UserViewSet, RestaurantViewSet, MenuViewSet, ReviewViewSet, ReservationViewSet
 from rest_framework import routers
 
--- project/project/urls.py
+++ project/project/urls.py
@@ -20,6 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catchtable.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/signup/', include('rest_auth.registration.urls')),
 ]
